2d33a2668a81dca75096c4dd7ebad619f8bd3c2c
rupesh <rpiwal02@gmail.com>
2024-03-30 19:34:31 +0530

    created  auth & error middleware

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,35 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+import express from "express";
+import userRouter from "./routes/user.js";
+import taskRouter from "./routes/task.js";
+import { config } from "dotenv";
+import cookieParser from "cookie-parser";
+import { errorMiddleware } from "./middlewares/error.js";
+import cors from "cors";
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+export const app = express();
+
+config({
+  path: "./data/config.env",
 });
+
+// Using Middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+// Using routes
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/task", taskRouter);
 
 app.get("/", (req, res) => {
-  res.render("login");
+  res.send("Nice working");
 });
 
-app.post("/login", (req, res) => {
-  res.cookie("token", "hello mf!!");
-  res.redirect("/");
-});
+// Using Error Middleware
+app.use(errorMiddleware);
